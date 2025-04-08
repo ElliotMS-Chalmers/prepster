3b2d7edc48e72b7425d336bcabd67fa1c777006f
marcus <marcus@synk.systems>
2025-04-09 00:23:10 +0200

    Fixed outdated import-path in pantry_repository_test.dart

--- test/model/repositories/pantry_repository_test.dart
+++ test/model/repositories/pantry_repository_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:prepster/model/entities/pantry_item.dart';
 import 'package:prepster/model/repositories/pantry_repository.dart';
 
 // Test for dummy-repository
