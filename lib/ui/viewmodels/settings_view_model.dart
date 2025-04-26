7ebb494bff0088e4a01a6fa69d8833eeafc3024e
marcus <marcus@synk.systems>
2025-04-26 04:16:09 +0200

    Features:
    1. Added persistent language to settings.dart (requires restart).
    2. Added *most* of the missing localization for settings-page.
    3. Rearranged localization-map to match order of key's between languages, and removed duplicates.

--- lib/ui/viewmodels/settings_view_model.dart
+++ lib/ui/viewmodels/settings_view_model.dart
@@ -5,22 +5,38 @@ import '../../model/repositories/settings_repository.dart';
 class SettingsViewModel extends ChangeNotifier {
   final SettingsRepository _repository;
   List<Map<String, Object>> _items = [];
+  String? _selectedLanguage;
+  List<String> _availableLanguages = [];
 
   SettingsViewModel(this._repository) {
     _initialize();
   }
 
   List<Map<String, Object>> getHousehold() => _items;
+  String? get selectedLanguage => _selectedLanguage;
+  List<String> get availableLanguages => _availableLanguages;
 
 
   Future<void> _initialize() async {
     // IMPORTANT need to initialize cache before using the repository!
     await _repository.initializeCache();
     await _loadItems();
+    await _loadAvailableLanguages();
+    await _loadSelectedLanguage();
   }
 
   Future<void> _loadItems() async {
     _items = await _repository.getHousehold();
+    notifyListeners();
+  }
+
+  Future<void> _loadAvailableLanguages() async {
+    _availableLanguages = _repository.getAvailableLanguages();
+    notifyListeners();
+  }
+
+  Future<void> _loadSelectedLanguage() async {
+    _selectedLanguage = await _repository.getSelectedLanguage();
     notifyListeners();
   }
 
@@ -56,5 +72,17 @@ class SettingsViewModel extends ChangeNotifier {
     await _repository.setDarkmodeOn(value);
   }
 
+  Future<void> setSelectedLanguage(String? languageCode) async {
+    if (languageCode != null && _selectedLanguage != languageCode) {
+      final success = await _repository.setLanguage(languageCode);
+      if (success) {
+        _selectedLanguage = languageCode;
+        notifyListeners();
+        //TODO: Add a UI refresh here
+      } else {
+        print('Failed to set language to $languageCode');
+      }
+    }
+  }
 }
 
