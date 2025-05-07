8935f72954de4046cf3016c334d2c76466c70bb1
marcus <marcus@synk.systems>
2025-05-08 00:55:17 +0200

    Disabled obsolete tests for json_storage_service_test.dart and pantry_repository_test.dart.

--- test/model/repositories/pantry_repository_test.dart
+++ test/model/repositories/pantry_repository_test.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:prepster/model/entities/pantry_item.dart';
@@ -156,3 +157,4 @@ void main() {
     print('PantryItems after delete: $retrievedList');
   });
 }
+*/
