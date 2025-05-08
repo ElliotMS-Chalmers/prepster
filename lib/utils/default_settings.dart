3d670d5091b610c9ae3252ae96752136019da247
Liv <liviab@chalmers.se>
2025-05-08 03:36:10 +0200

    added STORAGE_FILES

--- lib/utils/default_settings.dart
+++ lib/utils/default_settings.dart
@@ -1,14 +1,15 @@
-
-
-
 const String DEFAULT_LANGUAGE = 'en';
 const bool DEFAULT_NOTIFICATIONS = false;
 const bool DEFAULT_DARKMODE_ENABLED = false;
-const String DEFAULT_NOTIFY_DAYS_BEFORE = '30';
+const int DEFAULT_NOTIFY_DAYS_BEFORE = 30;
 const List<String> SUPPORTED_LANGUAGES = ['en', 'sv'];
 const int MAXIMUM_ALLOWED_NAME_LENGTH = 100;
+const String TIMEZONE = 'Europe/Stockholm';
 
 const String TRANSLATIONS_PATH = 'assets/translations';
 
-
-
+const Set<String> STORAGE_FILES = {
+  'pantry_data.json',
+  'medical_data.json',
+  'equipment_data.json',
+};
